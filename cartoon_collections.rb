d73cd734e894b678324701213c764aa1c5755016
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-25 21:05:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,11 +20,14 @@ def long_planeteer_calls(planeteer_calls)
   if planeteer_calls.count > 4
     true
   end
-  
+
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
+  planeteer_calls.find do |str|
+    str == valid_calls.each
+  end
 end
